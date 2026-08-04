3165cb38681deac3b5a181bba733cd2ccd66be7d
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-08-04 07:10:52 +0530

    refactor: reorganize app.js, added session and passport config/setup

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const path = require('node:path');
 const express = require('express');
-const app = express()
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
+const passport = require('passport');
+
 require("dotenv").config();
+
+const pool = require("./db/pool");
+
+//passport configuration
+require('./middleware/passport')
+
+const app = express()
 
 //view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -9,10 +19,35 @@ app.set('view engine', 'ejs');
 
 //middlewares
 app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
+//session setup
+app.use(session({
+  store: new pgSession({
+    pool,
+    createTableIfMissing: true,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+}));
+
+//passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Make current user available in every EJS view
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 //routes
 // const indexRouter = require("./routes/indexRouter");
+
+app.get("/", (Req, res) => {
+  res.redirect("auth/login");
+})
 
 const authRouter = require("./routes/authRouter");
 app.use("/auth", authRouter);
@@ -20,11 +55,9 @@ app.use("/auth", authRouter);
 const messageRouter = require("./routes/messageRouter");
 app.use("/msg", messageRouter);
 
-const port = 3000
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//server
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`server app listening on port ${PORT}`)
 })
